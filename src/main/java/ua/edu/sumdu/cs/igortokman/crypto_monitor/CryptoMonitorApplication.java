9632a079dceab15263dcaf60486d4fbd4573b38f
Ihor Tokman <Ihor.Tokman@portaone.com>
2020-06-07 13:27:54 +0300

    initial commit

---
+++ src/main/java/ua/edu/sumdu/cs/igortokman/crypto_monitor/CryptoMonitorApplication.java
@@ -0,0 +1,13 @@
+package ua.edu.sumdu.cs.igortokman.crypto_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/edu/sumdu/cs/igortokman/crypto_monitor/CryptoMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.edu.sumdu.cs.igortokman.crypto_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
